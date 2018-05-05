27ebf1156276f14b2f4f197dcc41e24d139bb202
Johniel <johniel.s.m@gmail.com>
2018-05-05 20:06:43 +0900

    panicじゃなくて異常終了するように

--- cmd/lavaloon/main.go
+++ cmd/lavaloon/main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/johniel/lavaloon/lavaloon"
 )
 
+func printUsage() {
+	fmt.Println("Usage: lavaloon FILEPATH")
+}
+
 func main() {
 	if len(os.Args) != 2 {
-		panic("usage: lavaloon FILEPATH")
+		printUsage()
+		os.Exit(1)
 	}
 
 	filepath := os.Args[1]
 
 	if _, err := os.Stat(filepath); err != nil {
-		panic("no such file: " + filepath)
+		fmt.Println("no such file: " + filepath)
+		os.Exit(1)
 	}
 
 	err := lavaloon.Transpile(filepath)
